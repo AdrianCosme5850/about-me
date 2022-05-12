5a58022e844ebbd2317d04aea4da0c329a295a79
Adrian Cosme-Halverson <adrian.cosme5850@gmail.com>
2022-05-11 20:01:00 -0700

    Added two more questions to the guessing game, and lists.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,12 @@
 'use strict';
+let correctAnswers = 0;
 let userName = prompt('What\'s your name?');
 alert('Welcome ' + userName + '!');
 let userGuess1 = prompt('Do you think I\'ve ever been outside the country? (Yes/No)');
 // console.log(userGuess1)
-// while(userGuess1.toLowerCase() != 'yes' || 'no'){
-//   alert('Please enter yes or no.')
-//   userGuess1 = prompt('Do you think I\'ve ever been outside the country? (Yes/No)')
-// }
 if (userGuess1.toLowerCase() === 'yes'){
   alert('That\'s correct!');
+  correctAnswers += 1;
 } else if (userGuess1.toLowerCase() === 'no'){
   alert('That\'s incorrect! I\'ve been to Iraqi, Afghanistan, and Mexico!'); }
 else {
@@ -19,6 +17,7 @@ let userGuess2 = prompt('Have I ever owned a ferret before?(Yes/No)');
 // console.log(userGuess2)
 if(userGuess2.toLowerCase() === 'yes'){
   alert('That is correct!');
+  correctAnswers += 1;
 } else if (userGuess2.toLowerCase() === 'no'){
   alert('That is incorrect! I\'ve owned three!');
 } else {
@@ -29,6 +28,7 @@ let userGuess3 = prompt('Do I drink too many monsters?');
 // console.log(userGuess3)
 if (userGuess3.toLowerCase() === 'yes'){
   alert('You are very correct');
+  correctAnswers += 1;
 } else if (userGuess3.toLowerCase()){
   alert('Incorrect! I drink an obscene amount!');
 } else {
@@ -41,6 +41,7 @@ if(userGuess4.toLowerCase() === 'yes'){
   alert('That is incorrect!');
 } else if (userGuess4.toLowerCase() === 'no'){
   alert('That is correct!');
+  correctAnswers += 1;
 } else {
   alert('Please enter yes or no.');
   userGuess4 = prompt('Do I have a tattoo?(Yes/No)');
@@ -51,8 +52,41 @@ if (userGuess5.toLowerCase() === 'yes'){
   alert('That is incorrect, I wish I owned one right now!');
 } else if(userGuess5.toLowerCase() === 'no'){
   alert('That is correct! I wish.');
+  correctAnswers += 1;
 } else {
   alert('Please enter yes or no.');
   userGuess5 = prompt('Do I own a motorcycle?(Yes/No)');
 }
-alert('Thanks for playing ' + userName + '!');
+let numberGuess = prompt('Guess a number! 1-10!');
+for (let x = 0; x < 3; x++) {
+  if (numberGuess === '5'){
+    alert('Correct!');
+    correctAnswers += 1;
+    break;
+  } else if (numberGuess < 5){
+    alert('Too low! Try again!');
+    numberGuess = prompt('Guess a number! 1-10!');
+  } else if (numberGuess > 5){
+    alert('Too high! Try again!');
+    numberGuess = prompt('Guess a number! 1-10!');
+  }
+  if(x === 2){
+    alert('The correct answer was 5. Thanks for playing!');
+  }
+}
+let userGuessCountry = prompt('Guess what country I\'ve been to before!');
+let countries = ['iraq', 'afghanistan', 'mexico'];
+for (let y = 0; y < 6; y++) {if (userGuessCountry === countries[0] || userGuessCountry === countries[1] || userGuessCountry === countries[2]){
+  alert('That\'s correct!');
+  correctAnswers += 1;
+  break;
+} else {
+  alert('Sorry, that\'s incorrect!');
+  userGuessCountry = prompt('Guess what country I\'ve been to before!');
+}
+if (y === 5){
+  alert('Sorry the correct answers were ' + countries[1] + ', ' + countries[0] + ', and ' + countries[2] + '.');
+}
+}
+
+alert('Thanks for playing ' + userName + '! You got ' + correctAnswers + ' right!');
